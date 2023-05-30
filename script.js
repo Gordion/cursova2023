2f2cb6dddbe207702298dc24a751b59c0c352a02
Gordion <Darthgordion@gmail.com>
2023-05-30 21:43:43 +0300

    updated model

--- script.js
+++ script.js
@@ -27,10 +27,6 @@ about.addEventListener("click", function () {
 let statsHTML = "";
 for (let i = 0; i < localStorage.length; i++) {
   let key = localStorage.key(i);
-  // statsHTML += JSON.parse(localStorage.getItem(key))[0];
-  // statsHTML += JSON.parse(localStorage.getItem(key))[1];
-  // statsHTML += JSON.parse(localStorage.getItem(key))[2];
-  // statsHTML += JSON.parse(localStorage.getItem(key))[3];
   statsHTML += `
   <tr>
   <td>${JSON.parse(localStorage.getItem(key))[0]}</td>
@@ -41,20 +37,6 @@ for (let i = 0; i < localStorage.length; i++) {
 }
 
 container.innerHTML += statsHTML;
-
-// for (let i = 0; i < localStorage.length; i++) {
-//   let key = localStorage.key(i);
-//   alert(`${key}: ${localStorage.getItem(key)}`);
-// }
-// for (let i in localStorage) {
-//   stats.innerHTML += JSON.parse(localStorage.getItem(localStorage.key(i)))[1];
-// }
-// for (let i in localStorage) {
-//   stats.innerHTML += JSON.parse(localStorage.getItem(localStorage.key(i)))[2];
-// }
-// for (let i in localStorage) {
-//   stats.innerHTML += JSON.parse(localStorage.getItem(localStorage.key(i)))[3];
-// }
 
 let progressBar = new ProgressBar.Circle("#progress", {
   color: "limegreen",
